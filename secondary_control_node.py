1b765aa86eafc9367ebded092f84aa68c3abe6d2
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-04-07 13:01:14 -0500

    secondary

--- secondary_control_node.py
+++ secondary_control_node.py
@@ -1,23 +1,37 @@
-import command
+import wifi
+import socketpool
+from secrets import secrets
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+import networking
+from node_config import *
+
+
+# Called when an MQTT message is received.
+# topic is the feed name the message was published on.
+# message is the contents of the message.
+def message_received(client, topic, message):
+    print(f"(SECONDARY) New message on topic {topic}: {message}")
+
+    # TODO: Parse the feed name and take action
+
+
+# TODO: set up networking, subscribe to feeds, and send initial feed messages
+
 
 # TODO: probably want some global variables here...
+networking.connect_to_network()
+networking.mqtt_initialize()
+networking.mqtt_connect(
+    [f"temperature-zone-{i + 1}" for i in range(num_zones)], message_received
+)
 
-# Called when a message is received over the socket
-def socket_message_received(msg):
-    # Parse the message as a Command
-    cmd = command.Command(msg=msg)
-    print(f'Command received: {cmd}, values: {cmd.values}')
 
-    # TODO: Check the command type and take action
-
-# TODO: Set up networking etc.
-
-# Perform regular secondary node control tasks
+# Run the regular primary control node tasks
 def loop():
     # TODO: throttle this loop? (i.e. don't run it every time)
 
-    #print("Executing secondary control node loop")
+    # print("Executing primary control node loop")
 
-    # TODO: maybe? anything to do here?
+    # TODO: Main temperature control logic
 
     pass
