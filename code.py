18b204792f352da8ef148e1e06b655f585e6acdb
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-04-08 15:37:02 -0500

    take a deep breath

--- code.py
+++ code.py
@@ -1,37 +1,43 @@
-import command
 from secrets_db import *
 import networking
 import time
 import temperature_measurement_node
 import node_config
 
+pre_functions = [
+    networking.loop,
+]
+post_functions = []
+
 if node_type == node_config.NODE_TYPE_PRIMARY:
     import primary_control_node
 
     def send():
-        msg = command.Command(command.TYPE_NONE, [21, 67], "you smart right?")
+        # msg = command.Command(command.TYPE_NONE, [21, 67], "you smart right?")
         networking.socket_send_message("what on earth?")
 
-    frequency = 10
-    functions = [
-        send,
-        networking.loop,
-        primary_control_node.loop,
-    ]
-
-    while True:
-        start_time = start = time.time()
-
-        for f in functions:
-            f()
-
-        end_time = start = time.time()
-        elapsed_seconds = end_time - start_time
+    def run(elapsed_seconds):
         iterations = round(elapsed_seconds * frequency)
 
         for _ in range(iterations):
             temperature_measurement_node.loop(1 / frequency)
+
+    frequency = 10
+    pre_functions.extend([send, primary_control_node.loop])
+
 elif node_type == node_config.NODE_TYPE_SECONDARY:
     import secondary_control_node
 
     print("zooweemama")
+
+while True:
+    start_time = start = time.time()
+
+    for f in pre_functions:
+        f()
+
+    end_time = start = time.time()
+    elapsed_seconds = end_time - start_time
+
+    for f in post_functions:
+        f(elapsed_seconds)
